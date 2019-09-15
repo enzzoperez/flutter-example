c47448be45cf20cc461ab37addcadc954fcf52f8
Enzzo Perez <enzzorp@gmail.com>
2019-09-15 20:12:33 -0300

    dice app finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,56 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-const primaryGreen = Color(0xff065E52);
-
-void main(){
-  runApp(MyApp());
+void main() {
+    return runApp(
+        MaterialApp(
+            home: Scaffold(
+                backgroundColor: Colors.red,
+                appBar: AppBar(
+                    title: Text('Dicee'),
+                    backgroundColor: Colors.red,
+                ),
+                body: DicePage(),
+            ),
+        ),
+    );
 }
 
-class MyApp extends StatelessWidget {
+class DicePage extends StatefulWidget {
+    @override
+    _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+    var leftDiceCount = 1;
+    var rightDiceCounter = 1;
     @override
     Widget build(BuildContext context) {
-        return MaterialApp(
-            home: Scaffold(
-                body: myBody(),
-            ),
-        );
-    }
-    
-    Widget myBody(){
-        return SafeArea(
-            child: Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        colors: [Color(0xff2b5876), Color(0xff4e4376)],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        stops: [0.2, 0.8]
-                    )
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        CircleAvatar(
-                            radius: 50.0,
-                            backgroundColor: Colors.red,
-                            child: Image(image: AssetImage('./assets/diamond.png'),),
+        return Center(
+            child: Row(
+                children: <Widget>[
+                    Expanded(
+                        child: FlatButton(
+                            onPressed: (){
+                                setState(() {
+                                    leftDiceCount = Random().nextInt(6) + 1;
+                                });
+                            },
+                            child: Image.asset('assets/img/dice$leftDiceCount.png')
                         ),
-                        Text('ENZO PEREZ', style: TextStyle(
-                            fontFamily: 'Rocksalt',
-                            fontSize: 20.0,
-                            color: Colors.white
-                        ),),
-                        Text('REACT NATIVE DEVELOPER', style: TextStyle(
-                            fontSize: 17.0,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                        ),),
-                        SizedBox(
-                            width: 250,
-                            child: Divider(
-                                color: Colors.white,
-                            ),
+                    ),
+                    Expanded(
+                        child: FlatButton(
+                            onPressed: (){
+                                setState(() {
+                                    rightDiceCounter = Random().nextInt(6) + 1;
+                                });
+                            },
+                            child: Image.asset('assets/img/dice$rightDiceCounter.png')
                         ),
-                        Card(
-                            margin: EdgeInsets.only(left: 35, right: 35, top: 10),
-                            child: ListTile(
-                                leading: Icon(Icons.phone),
-                                title: Text('391-239087'),
-                            ),
-                        ),
-                        Card(
-                            margin: EdgeInsets.only(left: 35, right: 35, top: 10),
-                            child: ListTile(
-                                leading: Icon(Icons.mail),
-                                title: Text('enzzo@gmail.com'),
-                            ),
-                        )
-                    ],
-                ),
+                    ),
+                ],
             ),
         );
     }
