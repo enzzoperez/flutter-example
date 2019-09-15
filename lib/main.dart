b827939ba77c755c6e39e7c14cddb4ab25f073e9
Enzzo Perez <enzzorp@gmail.com>
2019-09-15 11:34:56 -0300

    mi card app finished

--- lib/main.dart
+++ lib/main.dart
@@ -12,42 +12,62 @@ class MyApp extends StatelessWidget {
     Widget build(BuildContext context) {
         return MaterialApp(
             home: Scaffold(
-                body: Container(
-                    color: Colors.teal,
-                    child: SafeArea(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                                Container(
-                                    width: 100,
-                                    color: Colors.red,
-                                ),
-                                Container(
-                                    width: 100,
-                                    child: Center(
-                                        child: Column(
-                                            mainAxisAlignment: MainAxisAlignment.center,
-                                            children: <Widget>[
-                                                Container(
-                                                    height: 100,
-                                                    color: Colors.yellow,
-                                                ),
-                                                Container(
-                                                    height: 100,
-                                                    color: Colors.green,
-                                                )
-                                            ],
-                                        ),
-                                    ),
-                                ),
-                                Container(
-                                    width: 100,
-                                    color: Colors.blueAccent,
-                                ),
-                            ],
+                body: myBody(),
+            ),
+        );
+    }
+    
+    Widget myBody(){
+        return SafeArea(
+            child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Color(0xff2b5876), Color(0xff4e4376)],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        stops: [0.2, 0.8]
+                    )
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                        CircleAvatar(
+                            radius: 50.0,
+                            backgroundColor: Colors.red,
+                            child: Image(image: AssetImage('./assets/diamond.png'),),
                         ),
-                    ),
-                )
+                        Text('ENZO PEREZ', style: TextStyle(
+                            fontFamily: 'Rocksalt',
+                            fontSize: 20.0,
+                            color: Colors.white
+                        ),),
+                        Text('REACT NATIVE DEVELOPER', style: TextStyle(
+                            fontSize: 17.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                        ),),
+                        SizedBox(
+                            width: 250,
+                            child: Divider(
+                                color: Colors.white,
+                            ),
+                        ),
+                        Card(
+                            margin: EdgeInsets.only(left: 35, right: 35, top: 10),
+                            child: ListTile(
+                                leading: Icon(Icons.phone),
+                                title: Text('391-239087'),
+                            ),
+                        ),
+                        Card(
+                            margin: EdgeInsets.only(left: 35, right: 35, top: 10),
+                            child: ListTile(
+                                leading: Icon(Icons.mail),
+                                title: Text('enzzo@gmail.com'),
+                            ),
+                        )
+                    ],
+                ),
             ),
         );
     }
