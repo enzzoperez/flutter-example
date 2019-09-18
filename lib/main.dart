078c2b26d36a2a991d0901cd6584254b16b3ccbe
Enzzo Perez <enzzorp@gmail.com>
2019-09-18 10:34:28 -0300

    xilophone app with only one sound, incomplete yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,27 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
-    return runApp(BallApp());
+    return runApp(MyApp());
 }
 
-class BallApp extends StatefulWidget {
-    @override
-    _BallAppState createState() => _BallAppState();
-}
-
-class _BallAppState extends State<BallApp> {
-    var countImg = 0;
-
-    void randomImage(){
-        setState(() {
-            countImg = Random().nextInt(5);
-        });
-    }
-
+class MyApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
+        final AudioCache player = AudioCache();
+
         return MaterialApp(
-            home: Scaffold(
-                appBar: AppBar(
-                    backgroundColor: Colors.teal[800],
-                ),
-                body: Column(
-                    children: <Widget>[
-                        Expanded(
-                            child: Container(
-                                decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                    begin: Alignment.topLeft,
-                                    end: Alignment.bottomRight,
-                                    stops: [0.3, 1],
-                                    colors: [Colors.green, Colors.blue])
-                                ),
-                                child: FlatButton(
-                                    child: Image.asset('assets/img/ball$countImg.png'),
-                                    onPressed: (){
-                                        randomImage();
-                                    },
-                                ),
-                            ),
-                        ),
-                    ],
+            home: Container(
+            color: Colors.white,
+              child: Center(
+                child: FlatButton(
+                    onPressed: (){
+                      player.play('note1.wav');
+                    },
+                    child: Text('NOTA 1', textDirection: TextDirection.ltr,)
                 ),
             ),
+          ),
         );
     }
 }
