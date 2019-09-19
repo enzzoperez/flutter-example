c8ff8d30ba26ca8e7e5b3de670f9fa7267055072
Enzzo Perez <enzzorp@gmail.com>
2019-09-19 01:38:42 -0300

    added change for challenge xilophone, finished

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,34 @@ void main() {
 class MyApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        final AudioCache player = AudioCache();
-
         return MaterialApp(
-            home: Container(
-            color: Colors.white,
-              child: Center(
-                child: FlatButton(
-                    onPressed: (){
-                      player.play('note1.wav');
-                    },
-                    child: Text('NOTA 1', textDirection: TextDirection.ltr,)
+            home: Scaffold(
+                body: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                        buttonSound(color: Colors.teal, nsound: 1),
+                        buttonSound(color: Colors.red, nsound: 2),
+                        buttonSound(color: Colors.deepOrangeAccent, nsound: 3),
+                        buttonSound(color: Colors.amberAccent, nsound: 4),
+                        buttonSound(color: Colors.cyan, nsound: 5),
+                        buttonSound(color: Colors.greenAccent, nsound: 6),
+                        buttonSound(color: Colors.indigo, nsound: 7),
+                    ],
                 ),
             ),
-          ),
+        );
+    }
+
+    Widget buttonSound({color: Color, nsound: int}){
+        final AudioCache player = AudioCache();
+        return Expanded(
+            child: FlatButton(
+                child: Container(),
+                onPressed: (){
+                    player.play('note$nsound.wav');
+                },
+                color: color,
+            )
         );
     }
 }
